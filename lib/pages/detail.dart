3355b6009369966b67687f682216f2cb52e23730
sanskar1306 <sanskarkabra1306@gmail.com>
2021-06-21 21:27:13 +0530

    DAY18

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -9,16 +9,46 @@ class DetailPage extends StatelessWidget {
   final Item catalog;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(),
-        backgroundColor:MyTheme.creamColor,
-        body:Column(children: [
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor:MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color:Colors.white,
+        child: ButtonBar(
+                buttonPadding: EdgeInsets.symmetric(horizontal: 8),
+                alignment: MainAxisAlignment.spaceBetween,
+                children: ["\$${catalog.price}".text.xl3.bold.make(),
+                ElevatedButton(
+                  style:ButtonStyle(
+                    backgroundColor:MaterialStateProperty.all(MyTheme.darkBluishColor)
+                    ),
+                  onPressed: (){},child:"Buy".text.make()).wh(100,50)
+                ],
+              ),
+      ),
+      body:SafeArea(
+        bottom:false,
+        child: Column(children: [
             Hero(
                tag: Key(catalog.id.toString()),
-              child: Image.network(catalog.image)),
-        ],).p16()
-      ),
+              child: Image.network(catalog.image)).h32(context),
+              Expanded(child:VxArc(
+                height:30.0,
+                edge: VxEdge.TOP,
+                arcType: VxArcType.CONVEY,
+                child: Container(
+                  color:Colors.white,
+                  width:context.screenWidth,
+                  child:Column(
+                    children: [
+                      catalog.name.text.xl4.color(MyTheme.darkBluishColor).bold.make(),
+                      catalog.desc.text.xl.textStyle(context.captionStyle).make(),
+                    ],
+                  ).py64()
+                ),
+              ))
+        ],),
+      )
     );
   }
 }
